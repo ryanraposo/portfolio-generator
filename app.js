870b7c9a20e422bcb1bea2d73d36afd5c31d176a
Ryan Raposo <raposo.ryan@gmail.com>
2022-06-23 11:25:45 -0400

    add page generation

---
+++ app.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template')
+
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+
+const [name, github] = profileDataArgs;
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
